03cbfb43d5dd10b8d5feeec7279833126d111466
siddiqui <aurangzaibsiddiqui@gmail.com>
2017-07-08 20:34:31 +0200

    inheritance

--- inheritance.cpp
+++ inheritance.cpp
@@ -1,36 +1,65 @@
+/*
+ access levels      public  protected   private
+ same class         yes     yes         yes
+ derived class      yes     yes
+ not members        yes
+
+ class derived_class: public base_class     --> public remains public in derived
+ class derived_class: protected base_class  --> public remains protected in derived
+ class derived_class: private base_class    --> public remains private in derived
+
+ in most use cases, derived class inherits base as public
+
+ public inheritance inherits all base parts except:
+        ctor and dtor
+        = operator
+        friends
+        private
+
+  derived class uses base class ctor to initialize base class properties
+ */
+
 #include <iostream>
 
-class polygon {
+using namespace std;
+
+template<class T>
+class Output {
+public:
+    inline static void print(const T &a) {
+        cout << a << endl;
+    }
+};
+
+class Polygon {
 protected:
     int width;
     int height;
 public:
-    inline void setValues(const int &w, const int &h) {
-        width = w;
-        height = h;
-    }
+    Polygon() : width(1), height(1) {}
+    Polygon(const int &w, const int &h) : width(w), height(h) {}
 };
 
-class rectangle : public polygon {
+class Rectangle : public Polygon, public Output<int> {
 public:
+    Rectangle(const int &w, const int &h) : Polygon(w, h) {}
     inline int area() {
         return width * height;
     }
 };
 
-class triangle : public polygon {
+class Triangle : public Polygon, public Output<double> {
 public:
-    inline float area() {
-        return (1 / 2) * width * height;
+    Triangle(const int &w, const int &h) : Polygon(w, h) {}
+    inline double area() {
+        return (1.0 / 2.0) * double(width) * double(height);
     }
 };
 
 int main() {
-    rectangle rec;
-    rec.setValues(2, 3);
-    triangle triag;
-    triag.setValues(5, 6);
-    cout << "area of rect: " << rec.area() << endl;
-    cout << "area of triag: " << triag.area() << endl;
+    Rectangle rec(2, 3);
+    Triangle triag(4, 5);
+    Rectangle::print(rec.area());
+    Triangle::print(triag.area());
     return 0;
 }
--- polymorphism.cpp
+++ polymorphism.cpp
@@ -2,4 +2,6 @@
  int A::b(int c) { }    --> method b of class A with param c
  a->b                   --> a is a pointer to an instance, used to access method b
  class A: public B {};  --> A inherits B publicly
+
+ ptr to derived class is type-compatible with ptr to base class
  */
