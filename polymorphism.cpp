ef4169c7cdf459c716deeaa0c86bd7935247f41f
siddiqui <aurangzaibsiddiqui@gmail.com>
2017-07-09 09:23:31 +0200

    renamed

--- polymorphism.cpp
+++
@@ -1,7 +0,0 @@
-/*
- int A::b(int c) { }    --> method b of class A with param c
- a->b                   --> a is a pointer to an instance, used to access method b
- class A: public B {};  --> A inherits B publicly
-
- ptr to derived class is type-compatible with ptr to base class
- */
