7bed9930b482f7345f226913d4fecec4304a0ce3
Siddiqui <aurangzaibsiddiqui@gmail.com>
2023-10-20 10:47:12 +0200

    2023.10.20 — Classes - 3

--- 06_Class.cpp
+++ 06_Class.cpp
@@ -53,7 +53,7 @@ Scope resolution operator (::)
 - To access class property/method from outside the class
 
 -------------------------------------
-*this* pointer
+this
 -------------------------------------
 - To access class property/method from inside the class
 - Inside class, property can be accessed by this->prop or directly prop
@@ -61,7 +61,7 @@ Scope resolution operator (::)
 -------------------------------------
 Constructor (ctor)
 -------------------------------------
-- Used to initialize properties of the class
+- To initialize class properties
 - Has no return type
 
 -------------------------------------
@@ -82,11 +82,17 @@ Syntax of a method
 const int class::method(const int& a) const{}
 |__ return type         |__ params    |__method can't be changed
 
+-------------------------------------
+Constant instance
+-------------------------------------
+- All properties will become read-only
+- Can only access constant methods
+
 */
 
 using namespace std;
 
-namespace NSClass1 {
+namespace NSClass {
 
 // ==========================================================================================================
 // Class
@@ -111,6 +117,7 @@ class Class {
     Structure<T, N> protected_structure;
 
    public:
+    static int num_static;                                                       // Static property
     Class();                                                                     // Default Constructor
     Class(const Class &);                                                        // Copy Constructor
     Class(const int &, const array<T, N> &, const vector<T> &, const string &);  // Constructor
@@ -120,17 +127,22 @@ class Class {
     const string get_string();                                                   // Getter
     Class operator+(const Class &);                                              // Overloading
     void operator=(const Class &);                                               // Overloading
+    static void func_static();                                                   // Static method
 };
+
+// Static Property
+template <typename T, size_t N>
+int NSClass::Class<T, N>::num_static = 999;
 
 // Default Constructor
 template <typename T, size_t N>
-NSClass1::Class<T, N>::Class() : num(0) {
+NSClass::Class<T, N>::Class() : num(0) {
     this->private_structure.arr.fill(0);
 }
 
 // Copy Constructor
 template <typename T, size_t N>
-NSClass1::Class<T, N>::Class(const Class &inst) : num(inst.num) {
+NSClass::Class<T, N>::Class(const Class &inst) : num(inst.num) {
     // Option 1
     private_structure.arr = inst.private_structure.arr;
     private_structure.vec = inst.private_structure.vec;
@@ -149,14 +161,14 @@ NSClass1::Class<T, N>::Class(const Class &inst) : num(inst.num) {
 
 // Constructor
 template <typename T, size_t N>
-NSClass1::Class<T, N>::Class(const int &num, const array<T, N> &arr, const vector<T> &vec, const string &str) {
+NSClass::Class<T, N>::Class(const int &num, const array<T, N> &arr, const vector<T> &vec, const string &str) {
     this->num = num;
     this->private_structure = {.arr = arr, .vec = vec, .str = str};
 }
 
 // Setters
 template <typename T, size_t N>
-void NSClass1::Class<T, N>::set_vector(const vector<T> &vec) {
+void NSClass::Class<T, N>::set_vector(const vector<T> &vec) {
     // remove old elements
     this->private_structure.vec.clear();
     // add new elements
@@ -165,33 +177,38 @@ void NSClass1::Class<T, N>::set_vector(const vector<T> &vec) {
     }
 }
 template <typename T, size_t N>
-void NSClass1::Class<T, N>::set_string(const string &str) {
+void NSClass::Class<T, N>::set_string(const string &str) {
     this->private_structure.str = str;
 }
 
 // Getters
 template <typename T, size_t N>
-const vector<T> NSClass1::Class<T, N>::get_vector() {
+const vector<T> NSClass::Class<T, N>::get_vector() {
     return this->private_structure.vec;
 }
 template <typename T, size_t N>
-const string NSClass1::Class<T, N>::get_string() {
+const string NSClass::Class<T, N>::get_string() {
     return this->private_structure.str;
 }
 
 // Overloading
 template <typename T, size_t N>
-NSClass1::Class<T, N> NSClass1::Class<T, N>::operator+(const Class &inst) {
+NSClass::Class<T, N> NSClass::Class<T, N>::operator+(const Class &inst) {
     Class new_inst;
     new_inst.num = this->num + inst.num;
     return new_inst;
 }
-
 template <typename T, size_t N>
-void NSClass1::Class<T, N>::operator=(const Class &inst) {
+void NSClass::Class<T, N>::operator=(const Class &inst) {
     this->num = num;
     this->private_structure = inst.private_structure;
     this->protected_structure = inst.protected_structure;
+}
+
+// Static method
+template <typename T, size_t N>
+void NSClass::Class<T, N>::func_static() {
+    cout << endl << Class::num_static << endl;
 }
 
 // ==========================================================================================================
@@ -200,8 +217,8 @@ void NSClass1::Class<T, N>::operator=(const Class &inst) {
 
 void Test() {
     // Instantiation
-    NSClass1::Class<int, 3> ins1;
-    NSClass1::Class<int, 3> ins2(33, (array<int, 3>){1, 2, 3}, (vector<int>){4, 5, 6}, "Hello World");
+    NSClass::Class<int, 3> ins1;
+    NSClass::Class<int, 3> ins2(33, (array<int, 3>){1, 2, 3}, (vector<int>){4, 5, 6}, "Hello World");
 
     // Getter Setter
     vector<int> vec1 = ins1.get_vector();
@@ -210,7 +227,7 @@ void Test() {
     vector<int> vec3 = ins1.get_vector();
 
     // Overloading
-    NSClass1::Class<int, 3> ins3 = ins1 + ins2;
+    NSClass::Class<int, 3> ins3 = ins1 + ins2;
     ins1 = ins2;
 
     // ins1 is not a reference of ins2
@@ -219,14 +236,33 @@ void Test() {
     vector<int> vec5 = ins1.get_vector();
     vector<int> vec6 = ins2.get_vector();
 
+    // Static property
+    NSClass::Class<int, 1>::func_static();
+    NSClass::Class<int, 1>::num_static = 998;
+    NSClass::Class<int, 1>::func_static();
+    ins1.num_static = 997;
+    ins1.func_static();
+
+    // Pointers
+    int var = 0;
+    int arr[] = {1, 2, 3, 4};
+    const int *var_ptr = &var;                 // Pointer to variable
+    const int *arr_ptr = arr;                  // Pointer to array
+    NSClass::Class<int, 3> *inst_ptr = &ins1;  // Pointer to instance
+    cout << var_ptr;                           // Access by Pointer
+    cout << arr_ptr[0];                        // Access by Pointer
+    ins1.get_vector();                         // Access by Instance
+    inst_ptr->get_vector();                    // Access by Pointer
+    (*inst_ptr).get_vector();                  // Access by Dereferenced Pointer
+
     // Debugger point
     return;
 }
 
-}  // namespace NSClass1
+}  // namespace NSClass
 
 //---------------------------------------------------------------------------------------------------------------
 int main() {
-    NSClass1::Test();
+    NSClass::Test();
     return 0;
 }
