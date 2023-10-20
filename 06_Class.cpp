a24d122fee31e8337dfae1c3e1266713d83d47d5
Siddiqui <aurangzaibsiddiqui@gmail.com>
2023-10-20 10:08:54 +0200

    2023.10.20 — Classes - 2

--- 06_Class.cpp
+++ 06_Class.cpp
@@ -6,13 +6,13 @@
 
 using namespace std;
 
-// ================================================================
+// ==========================================================================================================
 // Link
-// ================================================================
+// ==========================================================================================================
 
-// ================================================================
+// ==========================================================================================================
 // Notes
-// ================================================================
+// ==========================================================================================================
 /*
 ------------------------------------
 Members
@@ -86,44 +86,147 @@ const int class::method(const int& a) const{}
 
 using namespace std;
 
-namespace Classes {
+namespace NSClass1 {
+
+// ==========================================================================================================
+// Class
+// ==========================================================================================================
 
 // Structure
 template <typename T, size_t N>
 struct Structure {
-    vector<T> vec = {};
-    array<T, N> arr = {};
-    string str = "hello world";
+    array<T, N> arr;
+    vector<T> vec;
+    string str;
 };
 
 // Class
 template <typename T, size_t N>
 class Class {
    private:
-    int a;
-    array<T, N> arr;
-    vector<T> vec;
-    Structure<int, 5> private_structure;
+    int num;
+    Structure<T, N> private_structure;
 
    protected:
-    Structure<double, 3> protected_structure;
+    Structure<T, N> protected_structure;
 
    public:
-    Class() : a(0) { arr.fill(1); };
-    Class(Structure<T, N>);
-    void set_vector() {
-        for (auto loop = 0; loop < this->private_structure.vec.size();
-             loop += 1) {
-            this->private_structure.vec.at(loop) = loop * 10;
-        }
-    }
-    vector<T> get_vector() { return this->private_structure.vec; }
-    string get_string();
+    Class();                                                                     // Default Constructor
+    Class(const Class &);                                                        // Copy Constructor
+    Class(const int &, const array<T, N> &, const vector<T> &, const string &);  // Constructor
+    void set_vector(const vector<T> &);                                          // Setter
+    void set_string(const string &);                                             // Setter
+    const vector<T> get_vector();                                                // Getter
+    const string get_string();                                                   // Getter
+    Class operator+(const Class &);                                              // Overloading
+    void operator=(const Class &);                                               // Overloading
 };
 
-}  // namespace Classes
+// Default Constructor
+template <typename T, size_t N>
+NSClass1::Class<T, N>::Class() : num(0) {
+    this->private_structure.arr.fill(0);
+}
 
+// Copy Constructor
+template <typename T, size_t N>
+NSClass1::Class<T, N>::Class(const Class &inst) : num(inst.num) {
+    // Option 1
+    private_structure.arr = inst.private_structure.arr;
+    private_structure.vec = inst.private_structure.vec;
+    private_structure.str = inst.private_structure.str;
+    // Option 2
+    this->private_structure.arr = inst.private_structure.arr;
+    this->private_structure.vec = inst.private_structure.vec;
+    this->private_structure.str = inst.private_structure.str;
+    // Option 3
+    this->private_structure = {
+        .arr = inst.private_structure.arr,
+        .vec = inst.private_structure.vec,
+        .str = inst.private_structure.str,
+    };
+};
+
+// Constructor
+template <typename T, size_t N>
+NSClass1::Class<T, N>::Class(const int &num, const array<T, N> &arr, const vector<T> &vec, const string &str) {
+    this->num = num;
+    this->private_structure = {.arr = arr, .vec = vec, .str = str};
+}
+
+// Setters
+template <typename T, size_t N>
+void NSClass1::Class<T, N>::set_vector(const vector<T> &vec) {
+    // remove old elements
+    this->private_structure.vec.clear();
+    // add new elements
+    for (const auto &itr : vec) {
+        this->private_structure.vec.push_back(itr);
+    }
+}
+template <typename T, size_t N>
+void NSClass1::Class<T, N>::set_string(const string &str) {
+    this->private_structure.str = str;
+}
+
+// Getters
+template <typename T, size_t N>
+const vector<T> NSClass1::Class<T, N>::get_vector() {
+    return this->private_structure.vec;
+}
+template <typename T, size_t N>
+const string NSClass1::Class<T, N>::get_string() {
+    return this->private_structure.str;
+}
+
+// Overloading
+template <typename T, size_t N>
+NSClass1::Class<T, N> NSClass1::Class<T, N>::operator+(const Class &inst) {
+    Class new_inst;
+    new_inst.num = this->num + inst.num;
+    return new_inst;
+}
+
+template <typename T, size_t N>
+void NSClass1::Class<T, N>::operator=(const Class &inst) {
+    this->num = num;
+    this->private_structure = inst.private_structure;
+    this->protected_structure = inst.protected_structure;
+}
+
+// ==========================================================================================================
+// Test
+// ==========================================================================================================
+
+void Test() {
+    // Instantiation
+    NSClass1::Class<int, 3> ins1;
+    NSClass1::Class<int, 3> ins2(33, (array<int, 3>){1, 2, 3}, (vector<int>){4, 5, 6}, "Hello World");
+
+    // Getter Setter
+    vector<int> vec1 = ins1.get_vector();
+    vector<int> vec2 = ins2.get_vector();
+    ins1.set_vector(vector<int>{99, 99, 99});
+    vector<int> vec3 = ins1.get_vector();
+
+    // Overloading
+    NSClass1::Class<int, 3> ins3 = ins1 + ins2;
+    ins1 = ins2;
+
+    // ins1 is not a reference of ins2
+    vector<int> vec4 = ins1.get_vector();
+    ins2.set_vector((vector<int>){-99, -99, -99});
+    vector<int> vec5 = ins1.get_vector();
+    vector<int> vec6 = ins2.get_vector();
+
+    // Debugger point
+    return;
+}
+
+}  // namespace NSClass1
+
+//---------------------------------------------------------------------------------------------------------------
 int main() {
-    Classes::Class<int, 5> ins();
-    ins.set_vector();
+    NSClass1::Test();
+    return 0;
 }
--- 99_Test.cpp
+++ 99_Test.cpp
@@ -1,45 +1,15 @@
 #include <iostream>
-#include <array>
 #include <vector>
+#include <array>
 
 using namespace std;
-
-template <typename T>
-void func_vec(const vector<T> &vec) {
-    cout << "Function: func_vec: " << vec.at(0) << endl;
+void func_abc(const vector<int> &vec, const string str) {
+    // element
+    cout << vec.at(1) << endl;
+    cout << str << endl;
 }
 
-template <typename T, size_t N>
-void func_arr(const array<T, N> &arr) {
-    cout << "Function: func_arr: " << arr.at(0) << endl;
-}
-
-void func_arr(const int *ptr_arr) {
-    cout << "Function: func_arr(ptr): " << ptr_arr[0] << endl;
-}
-
-
-int main(){
-    array  arr = {1,2,3,4,5};
-    vector vec = {1,2,3,4,5};
-
-    cout << arr.front() << endl;
-    cout << arr.begin() << endl;
-    cout << arr.back()  << endl;
-    cout << arr.end()   << endl;
-
-    for (const auto &iterator : vec) {
-        cout << iterator << endl;
-    }
-
-    func_vec(vec);
-    func_arr(arr);
-    func_arr(arr.begin());
-
-    int my_var          = 123;
-    int *ptr            = &my_var; // nonconst a/d, nonconst val
-    const int *ptr      = &my_var; // nonconst a/d, const val
-    int *const ptr      = &my_var; // const a/d, const val
-    const int *const ptr= &my_var; // const a/d, const val
-
+int main() {
+    int arr[] = {1, 2, 3, 4};
+    func_abc((vector<int>){1, 2, 3, 4}, "Hello World");
 }
