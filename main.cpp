1b2b0c33204ee0d95a810400329a55f467cace83
Siddiqui <aurangzaibsiddiqui@gmail.com>
2024-01-30 21:40:44 +0100

    CMake Test File

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-
 using namespace std;
-#include <xtensor/xarray.hpp>
 
 int main() {
-    xt::xarray<double>::shape_type shape = {2, 3};
-    xt::xarray<double> a0(shape);
-    xt::xarray<double> a1(shape, 2.5);
-    xt::xarray<double> a2 = {{1., 2., 3.}, {4., 5., 6.}};
-    auto a3 = xt::xarray<double>::from_shape(shape);
-    int d = 0;
+    cout << endl;
+    cout << "---------------------------------" << endl;
+    cout << "CMake configuration successful" << endl;
+    cout << "---------------------------------" << endl;
+    cout << endl;
 }
